7aad45df463b041c7ae14d57593e2c8459edb32e
Jordon Bruton <jordon.bruton@baked.webseccorp.com>
2021-06-15 13:24:36 -0700

    Adding Foundation of MainWindow

--- BuildAComputerDesktopApp/BuildAComputerDesktopApp/Views/MainWindow.xaml.cs
+++ BuildAComputerDesktopApp/BuildAComputerDesktopApp/Views/MainWindow.xaml.cs
@@ -13,12 +13,13 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace BuildAComputerDesktopApp
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow 
     {
         public MainWindow()
         {
