12e3fd8a9a1926e12c0fcf9a93f441e9604e7fe3
Jordon Bruton <jordon.bruton@baked.webseccorp.com>
2021-06-16 14:52:25 -0700

    adding two new windows

---
+++ BuildAComputerDesktopApp/BuildAComputerDesktopApp/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BuildAComputerDesktopApp.ViewModels;
+using System.Windows.Input;
+
+namespace BuildAComputerDesktopApp.ViewModels
+{
+    class MainWindowViewModel
+    {
+        #region ICommands
+
+        #endregion
+    }
+}
