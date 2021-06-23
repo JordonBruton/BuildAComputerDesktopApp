91b9f2bf218f2ce5881340454c3ef5d21385c0ab
Jordon Bruton <jordon.bruton@baked.webseccorp.com>
2021-06-23 14:19:02 -0700

    Adding database & database schema

---
+++ BuildAComputerDesktopApp/BuildAComputerDesktopApp/PcPartsDatabaseClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using sys
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildAComputerDesktopApp
+{
+    class PcPartsDatabaseClass
+    {
+        
+    }
+}
